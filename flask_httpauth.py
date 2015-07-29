cdb16774c1a35e3ccac626432b83285c916564bd
Steven Osborn <osborn.steven@gmail.com>
2015-07-28 18:40:01 -0700

    Hack for Cordova
    For whatever reason webkit shits when it sees www-authenticate headers and ajax requests appear to hang and not return a response.  Not including the www-authenticate header allows the client to receive a 401 error and act appropriately, even if it is not really standard.

--- flask_httpauth.py
+++ flask_httpauth.py
@@ -37,8 +37,8 @@ class HTTPAuth(object):
             if type(res) == str:
                 res = make_response(res)
                 res.status_code = 401
-            if 'WWW-Authenticate' not in res.headers.keys():
-                res.headers['WWW-Authenticate'] = self.authenticate_header()
+#            if 'WWW-Authenticate' not in res.headers.keys():
+#                res.headers['WWW-Authenticate'] = self.authenticate_header()
             return res
         self.auth_error_callback = decorated
         return decorated
